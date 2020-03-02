b7f97b891bce157f7c50177c5a435cf909b5d66a
Anaida <anaidashaginyan@yandex.ru>
2020-03-02 23:14:42 +0300

    reverse_vowels

---
+++ string/reverse_vowels.py
@@ -0,0 +1,22 @@
+def reverse_vowels(s):
+  vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+  vowels_s = ''
+  vowels_i = []
+  s_l = []
+  for i, letter in enumerate(s):
+    s_l.append(letter)
+    if letter in vowels:
+      vowels_i.append(i)
+      vowels_s += letter
+  
+  vowels_s = vowels_s[::-1]
+  for k, ind in enumerate(vowels_i):
+    s_l[ind] = vowels_s[k]
+
+  s = ''
+  for letter in s_l:
+    s+=letter
+  return s
+
+s = "aA"
+reverse_vowels(s)
